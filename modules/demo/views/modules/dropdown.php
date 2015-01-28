d4a680fcf4657325f6cc614ee9879caab510e504
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-01-28 03:05:20 +0300

    add dropdown demo partially

--- modules/demo/views/modules/dropdown.php
+++ modules/demo/views/modules/dropdown.php
@@ -1,1 +1,87 @@
-Development
+<?php
+/**
+ * @var View $this
+ */
+
+use app\modules\demo\widgets\Example;
+use yii\web\View;
+use Zelenin\yii\SemanticUI\Elements;
+use Zelenin\yii\SemanticUI\modules\Dropdown;
+use Zelenin\yii\SemanticUI\modules\Select;
+
+$this->title = 'Dropdown';
+
+Elements::header($this->title);
+?>
+
+<?= Example::widget([
+    'demo' => Select::widget([
+        'name' => 'select1',
+        'selection' => 3,
+        'search' => false,
+        'fluid' => true,
+        'disabled' => false,
+        'items' => [
+            '' => '---',
+            '1' => 'Label 1',
+            '2' => 'Label 2',
+            '3' => 'Label 3'
+        ]
+    ]),
+    'code' => <<<PHP
+use Zelenin\yii\SemanticUI\modules\Select;
+echo Select::widget([
+    'name' => 'select1',
+    'selection' => 3,
+    'search' => false,
+    'fluid' => true,
+    'disabled' => false,
+    'items' => [
+        '' => '---',
+        '1' => 'Label 1',
+        '2' => 'Label 2',
+        '3' => 'Label 3'
+    ]
+]);
+PHP
+]) ?>
+
+<?= Example::widget([
+    'demo' => Dropdown::widget([
+        'name' => 'select1',
+        'selection' => 3,
+        'search' => false,
+        'fluid' => true,
+        'disabled' => false,
+        'defaultText' => 'Select an option',
+        'items' => [
+            '',
+            'Categories' => [
+                '1' => 'Clothing',
+                '2' => 'Home Goods',
+                '3' => 'Bedroom'
+            ],
+            null,
+            'Order' => [
+                '4' => 'Status',
+                '5' => 'Cancellations'
+            ]
+        ]
+    ]),
+    'code' => <<<PHP
+use Zelenin\yii\SemanticUI\modules\Select;
+echo Select::widget([
+    'name' => 'select1',
+    'selection' => 3,
+    'search' => false,
+    'fluid' => true,
+    'disabled' => false,
+    'items' => [
+        '' => '---',
+        '1' => 'Label 1',
+        '2' => 'Label 2',
+        '3' => 'Label 3'
+    ]
+]);
+PHP
+]) ?>
