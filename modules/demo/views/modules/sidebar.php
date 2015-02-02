8ab352039e28bfee44dd3ff131ad8060c6698f66
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-02-02 13:22:25 +0300

    fix

--- modules/demo/views/modules/sidebar.php
+++ modules/demo/views/modules/sidebar.php
@@ -16,17 +16,13 @@ Elements::header($this->title);
 <?= Example::widget([
     'demo' => Sidebar::widget([
             'id' => ($sidebarId = 'sidebar-id'),
-            'visible' => true,
             'content' => $this->context->renderPartial('@app/modules/demo/views/layouts/_menu')
-
         ]) . Elements::button('Toggle', ['class' => 'left floated', 'id' => ($buttonId = 'toggle-button')]) . $this->registerJs('jQuery("#' . $buttonId . '").click(function() { jQuery("#' . $sidebarId . '").sidebar("toggle"); });'),
     'code' => <<<'PHP'
 use Zelenin\yii\SemanticUI\modules\Sidebar;
 echo Sidebar::widget([
     'id' => ($sidebarId = 'sidebar-id'),
-    'visible' => true,
     'content' => $this->context->renderPartial('@app/modules/demo/views/layouts/_menu')
-
 ]) . Elements::button('Toggle', ['class' => 'left floated', 'id' => ($buttonId = 'toggle-button')]) . $this->registerJs('jQuery("#' . $buttonId . '").click(function() { jQuery("#' . $sidebarId . '").sidebar("toggle"); });');
 PHP
 ]) ?>
