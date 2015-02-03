976a8115600a489242811db0e46bc6417fa77272
Aleksandr Zelenin <aleksandr@zelenin.me>
2015-02-04 01:58:38 +0300

    add Breadcrumb demo

--- modules/demo/views/collections/breadcrumb.php
+++ modules/demo/views/collections/breadcrumb.php
@@ -1,1 +1,52 @@
-Development
+<?php
+/**
+ * @var View $this
+ */
+
+use app\modules\demo\widgets\Example;
+use yii\web\View;
+use Zelenin\yii\SemanticUI\collections\Breadcrumb;
+use Zelenin\yii\SemanticUI\Elements;
+
+$this->title = 'Accordion';
+
+Elements::header($this->title);
+?>
+
+<?= Example::widget([
+    'demo' => Breadcrumb::widget([
+        'divider' => Breadcrumb::DIVIDER_CHEVRON,
+        'items' => [
+            [
+                'label' => 'Menu',
+                'url' => ['/demo/collections/menu']
+            ],
+            [
+                'label' => 'Message',
+                'url' => ['/demo/collections/message']
+            ],
+            [
+                'label' => 'Breadcrumb'
+            ]
+        ]
+    ]),
+    'code' => <<<PHP
+use Zelenin\yii\SemanticUI\modules\Breadcrumb;
+echo Breadcrumb::widget([
+    'divider' => Breadcrumb::DIVIDER_CHEVRON,
+    'items' => [
+        [
+            'label' => 'Menu',
+            'url' => ['/demo/collections/menu']
+        ],
+        [
+            'label' => 'Message',
+            'url' => ['/demo/collections/message']
+        ],
+        [
+            'label' => 'Breadcrumb'
+        ]
+    ]
+]);
+PHP
+]) ?>
